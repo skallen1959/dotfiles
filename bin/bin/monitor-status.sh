d51428b34e6197117a0ea019988671d88cdafb8a
Anders Karlsson <anders.karlsson@istudios.se>
2025-07-29 09:03:06 +0200

    Too many changes to remember

--- bin/bin/monitor-status.sh
+++ bin/bin/monitor-status.sh
@@ -8,13 +8,13 @@ EDP_ACTIVE=$(echo "$output" | grep -A18 "Monitor eDP-1" | grep -q "disabled: fal
 HDMI_ACTIVE=$(echo "$output" | grep -A18 "Monitor HDMI-A-1" | grep -q "disabled: false" && echo "HDMI-A-1" || echo "")
 
 if [ -n "$EDP_ACTIVE" ] && [ -n "$HDMI_ACTIVE" ]; then
-    TEXT="Both"
+    TEXT="  Both"
 elif [ -n "$EDP_ACTIVE" ]; then
-    TEXT="Int"
+    TEXT="  Int"
 elif [ -n "$HDMI_ACTIVE" ]; then
-    TEXT="Ext"
+    TEXT="  Ext"
 else
-    TEXT="No screen"
+    TEXT="  No screen"
 fi
 
 # Returnera enkel sträng
